5d64c4e6cf15b188223d406a322cb3cb94579d0c
Manuel Wagner <manuel.wagner@leftshift.one>
2020-01-16 09:58:26 +0100

    added scss

--- next.config.js
+++ next.config.js
@@ -1,2 +1,30 @@
 const withImages = require('next-images');
-module.exports = withImages();
+const path = require('path');
+
+module.exports = withImages({
+    webpack: (config) => {
+        config.module.rules.push(
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'emit-file-loader',
+                        options: {
+                            name: 'dist/[path][name].[ext].js',
+                        },
+                    },
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            babelrc: false,
+                            extends: path.resolve(__dirname, './.babelrc'),
+                        },
+                    },
+                    'styled-jsx-css-loader',
+                ],
+            }
+        );
+
+        return config;
+    }
+});
