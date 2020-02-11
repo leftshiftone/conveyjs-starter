7c27580afc8a185959ca81dc2df7ff145d4189da
Michael Mair <michael.mair@leftshift.one>
2020-02-11 20:32:52 +0100

    fixed compilation

---
+++ components/custom/Template.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function () {
+    return (
+            <div className="alert alert-primary" role="alert">
+                No gaiaUrl/gaiaIdentityId set
+            </div>
+    )
+}
